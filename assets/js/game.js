e8ca2856e2d8a1fa0639dc860fff9de0f1feabbd
NickdevC <nick_coles@msn.com>
2022-06-04 19:45:50 +0000

    Add js to allow user to enter and upload high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -182,10 +182,26 @@ const maxHighScores = 5;
 
 finalScore.innerText = mostRecentScore;
 
-userName.addEventListener('keyup', () ==> {
+userName.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !userName.value;
 })
 
 saveHighScore = e => {
-    e.preventDefault()
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: userName.value
+    }
+
+    highScores.push(score);
+
+    highScores.sort((a,b) => {
+        return b.score - a.score;
+    })
+
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
 }
